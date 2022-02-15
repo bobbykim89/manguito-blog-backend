8834944d9e54f72f46a9b9bd5d4b152a148ee0f3
bobbykim89 <bobby.sihun.kim@gmail.com>
2022-02-14 17:41:35 -0700

    add files to repo

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '687d3c837cb1ff384fb937de724cf96b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '00d6c61e-ef7a-4ca6-8221-6762279f7ff3'
+};
